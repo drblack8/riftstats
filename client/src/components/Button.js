b73a7235562587b9444119596b056304f71f0f3e
Dan Black <danielb0318@gmail.com>
2020-11-04 12:48:23 -0500

    added button css

---
+++ client/src/components/Button.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Button.css'
+
+const styles = [
+    'butn-primary',
+    'btn-outline'
+]
+
+const sizes = [
+    'btn--medium',
+    'btn--large'
+]
+
+export const Button = ({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    buttonSize
+}) => {
+    const checkButtonStyle = style.includes(buttonStyle) ? buttonStyle : styles[0]
+    const checkButtonSize = sizes.includes(buttonSize) ? buttonSize : sizes[0]
+    return (
+        <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>{children}</button>
+    )
+}
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,15 +4,20 @@ import "./Navbar.css";
 
 const Navbar = () => {
   const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+      setClicked(true)
+  }
+
   return (
     <nav className="NavbarItems">
       <h1 className="navbar-logo">
         RiftStats<i className="fab fa-react"></i>
       </h1>
-      <div className="menu-icon">
+      <div className="menu-icon" onClick={handleClick}>
           <i className={clicked ? "fas fa-times": "fas fa-bars"}></i>
       </div>
-      <ul>
+      <ul className={clicked ? "nav-menu active" : "nav-menu"}>
         {MenuItems.map((item, idx) => {
           return (
             <li key={idx}>
