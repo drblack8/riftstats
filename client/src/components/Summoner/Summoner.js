05005cc1eda64172b453beb8e2b9490c3613fce5
Dan Black <danielb0318@gmail.com>
2020-11-04 19:03:13 -0500

    starting to play with riot api

--- client/src/components/Summoner/Summoner.js
+++ client/src/components/Summoner/Summoner.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Summoner.css'
 import SearchBar from "../SearchBar/SearchBar";
 
 const Summoner = () => {
+    const [summoner, setSummoner] = useState({
+        name: 'dan'
+    })
     return (
         <div className='sum-page'>
-            <SearchBar />
-            <h1>help</h1>
+            <div className='sum-page-search'>
+                <SearchBar />
+            </div>
+            <div className='sum-info'>
+                <h1>
+                    {summoner.name}
+                </h1>
+            </div>
         </div>
     )
 }
