1af793fda2afc07e13e1daf160438a004d52b2ef
Dan Black <danielb0318@gmail.com>
2020-11-04 20:08:49 -0500

    need to fix some url stuff

--- client/src/components/Summoner/Summoner.js
+++ client/src/components/Summoner/Summoner.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Summoner.css'
 import SearchBar from "../SearchBar/SearchBar";
 
 const Summoner = () => {
-    const [summoner, setSummoner] = useState({
-        name: 'dan'
-    })
+    const [summoner, setSummoner] = useState(null)
+    const [url, setUrl] = useState(null)
+    useEffect(() => {
+        setUrl(window.location.href.split('userName=')[1])
+        console.log(url);
+        const data = fetch(`/api/summoner/userName=${url}`)
+
+    }, [null])
     return (
         <div className='sum-page'>
             <div className='sum-page-search'>
@@ -13,7 +18,7 @@ const Summoner = () => {
             </div>
             <div className='sum-info'>
                 <h1>
-                    {summoner.name}
+                    {/* {summoner.name} */} wat
                 </h1>
             </div>
         </div>
