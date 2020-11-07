4840666e82fa356ba61e2d8a1444c12c7d8c08ab
Dan Black <danielb0318@gmail.com>
2020-11-06 21:09:00 -0500

    found a good place to stop for now, desigend draft models and need to set up inserts and queries

--- starter_app/api/summoner_routes.py
+++ starter_app/api/summoner_routes.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from flask import Blueprint
+from starter_app.models import Match, Summoner
 
 
 summoner_routes = Blueprint('summoner', __name__)
@@ -8,11 +9,9 @@ summoner_routes = Blueprint('summoner', __name__)
 
 @summoner_routes.route('/info/<username>')
 def get_sum(username):
-    print('WEEEEWOOOO')
     key = os.environ.get('RIOT_API_KEY')
     account_url = f'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{username}?api_key={key}'
     account_id = requests.get(account_url).json()['accountId']
-    print(account_id)
     res_url = f'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/{account_id}?endIndex=20&api_key={key}'
     response = requests.get(res_url)
     return response.json()
