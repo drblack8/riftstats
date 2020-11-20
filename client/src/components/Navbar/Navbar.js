385d9864044c2eaa7a769d3ba52175e07f23885c
Dan Black <danielb0318@gmail.com>
2020-11-20 00:25:15 -0500

    fixed stuff

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { Button } from "../Button/Button";
-import { LoggedMenu } from "./LoggedMenu";
 import { MenuItems } from "./MenuItems";
 import NestedSearch from "./NestedSearch/NestedSearch";
 import "./Navbar.css";
@@ -9,7 +8,6 @@ import MenuSearch from "./MenuSearch/MenuSearch";
 
 const Navbar = () => {
   const [clicked, setClicked] = useState(false);
-  const [logged, setLogged] = useState(false);
   const [home, setHome] = useState(false)
   const menu = useRef();
   const search = useRef();
@@ -20,8 +18,6 @@ const Navbar = () => {
   };
   const location = useLocation()
 
-  setLogged(false)
-
   useEffect(() => {
     if (location.pathname === '/') {
       setHome(true)
@@ -30,7 +26,6 @@ const Navbar = () => {
     }
   },[location])
 
-  if (!logged) {
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">
@@ -61,39 +56,6 @@ const Navbar = () => {
         <NavLink to='/login'><Button>Login</Button></NavLink>
       </nav>
     );
-  } else {
-    return (
-      <nav className="NavbarItems">
-        <h1 className="navbar-logo">
-          RiftStats
-        </h1>
-        <div className="navbar-search">
-            <NestedSearch />
-        </div>
-        <div className="menu-icon" onClick={handleClick}>
-          <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
-        </div>
-        <ul ref={menu} className="nav-menu hidden">
-          <div ref={search} key="search" className="nav-menu-search">
-            <MenuSearch />
-          </div>
-          {LoggedMenu.map((item, idx) => {
-            return (
-              <li key={idx}>
-                <NavLink className={item.cName} to={item.url}>
-                  {item.title}
-                </NavLink>
-              </li>
-            );
-          })}
-        </ul>
-        <div className="btn-container">
-        <div className="shine"></div>
-        <NavLink to='/'><Button>Logout</Button></NavLink>
-        </div>
-      </nav>
-    );
-  }
 };
 
 export default Navbar;
