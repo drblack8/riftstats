5b2e314cc557b1cb104207fa8135440d1c76f0b5
Dan Black <danielb0318@gmail.com>
2021-11-28 19:02:09 -0500

    lots of progress on new update

--- starter_app/api/match_routes.py
+++ starter_app/api/match_routes.py
@@ -2,13 +2,14 @@ import os
 import json
 import requests
 from flask import Blueprint, jsonify
+from requests.exceptions import URLRequired
 from sqlalchemy.exc import SQLAlchemyError
 from starter_app.models import Match, db
 
 
 match_routes = Blueprint('match', __name__)
 base_url = 'https://na1.api.riotgames.com/lol'
-
+new_base_url = 'https://americas.api.riotgames.com/lol'
 
 @match_routes.route('/post/<summoner>')
 def update_matches(summoner):
@@ -17,38 +18,52 @@ def update_matches(summoner):
         f'{base_url}/summoner/v4/summoners/by-name/'
         f'{summoner}?api_key={key}'
     )
+
     summoner = requests.get(account_url)
     account_id = summoner.json()['accountId']
-    matches_url = (
-        f'{base_url}/match/v5/matchlists/by-account/'
-        f'{account_id}?endIndex=50&api_key={key}'
+    puid = summoner.json()['puuid']
+    # print("accountid:", account_id)
+    # print("SUMMONER:", puid)
+    match_arr_url = (
+        f'{new_base_url}/match/v5/matches/by-puuid/'
+        f'{puid}/ids?start=0&count=20&api_key={key}'
     )
-    print("RIGHT HERE:", requests.get(matches_url).json())
 
-    matches = requests.get(matches_url).json()['matches']
-    # print("match:", matches)
-    for match in matches:
-        # print("match:", match)
-        matchId = match['gameId']
-        match_url = f'{base_url}/match/v5/matches/{matchId}?api_key={key}'
+    # print("RIGHT HERE:", requests.get(matches_url).json())
+    matcharr = requests.get(match_arr_url).json()
+    # for e in matcharr:
+    #     matches_url = (
+    #         f'{new_base_url}/match/v5/matches/{e}'
+    #         f'?api_key={key}'
+    #     )
+    #     requests.get(matches_url)
+    # # matches = requests.get(matches_url).json()
+    # print("ARRAY:", matcharr)
+
+    # matches = []
+    for match in matcharr:
+
+        match_url = f'{new_base_url}/match/v5/matches/{match}?api_key={key}'
         try:
             response = requests.get(match_url)
-            game_id = response.json()['gameId']
-            platform_id = response.json()['platformId']
-            game_creation = response.json()['gameCreation']
-            game_duration = response.json()['gameDuration']
-            queue_id = response.json()['queueId']
-            season_id = response.json()['seasonId']
-            game_mode = response.json()['gameMode']
-            teams = json.dumps(response.json()['teams'])
-            part = json.dumps(response.json()['participants'])
-            pi = json.dumps(response.json()['participantIdentities'])
+            if match == 'NA1_4113395636':
+                print(response.json()['info']['gameId'])
+            game_id = response.json()['info']['gameId']
+            platform_id = response.json()['info']['platformId']
+            game_creation = response.json()['info']['gameCreation']
+            game_duration = response.json()['info']['gameDuration']
+            queue_id = response.json()['info']['queueId']
+            # season_id = response.json()['info']['seasonId']
+            game_mode = response.json()['info']['gameMode']
+            teams = json.dumps(response.json()['info']['teams'])
+            part = json.dumps(response.json()['info']['participants'])
+            pi = json.dumps(response.json()['info']['participantIdentities'])
             try:
                 new_match = Match(gameId=game_id,
                                   platformId=platform_id,
                                   gameCreation=game_creation,
                                   gameDuration=game_duration,
-                                  queueId=queue_id, seasonId=season_id,
+                                  queueId=queue_id, seasonId='11',
                                   gameMode=game_mode, teams=teams,
                                   participants=part,
                                   participantIdentities=pi)
