a128cd70920e537dc7f619db352274d015fbac07
Dan Black <danielb0318@gmail.com>
2022-01-10 13:55:04 -0500

    added quick demo page and style

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Home from "./components/Home/Home";
 import Login from "./components/User/Login";
 import Navbar from "./components/Navbar/Navbar";
 import Summoner from "./components/Summoner/Summoner";
-import Demo from "./components/Demo";
+import Demo from "./components/Demo/Demo";
 
 function App() {
   return (
---
+++ client/src/components/Demo/Demo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Demo.css";
+
+const Demo = () => {
+    return(
+    <div className="demo">
+		<iframe width="784" height="441" src="https://www.youtube.com/embed/gR1LV6MIYZI?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</div>
+    )
+}
+
+export default Demo;
