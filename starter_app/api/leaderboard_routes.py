ebc5d5a1c155b09dbe0fc5160da39ea226000e00
Dan Black <danielb0318@gmail.com>
2022-01-10 13:39:04 -0500

    adding demo page

--- starter_app/api/leaderboard_routes.py
+++ starter_app/api/leaderboard_routes.py
@@ -17,8 +17,17 @@ def leaderboards():
         f'/CHALLENGER/I?page=1&api_key={key}'
     )
 
-    players = requests.get(lb_url).json()
+    extra_url = (
+        f'{base_url}/league-exp/v4/entries/RANKED_SOLO_5x5'
+        f'/GRANDMASTER/I?page=1&api_key={key}'
+    )
 
+    players = requests.get(lb_url).json()
+    if len(players) < 10:
+        extra = requests.get(extra_url).json()
+        for i in extra:
+            players.append(i)
+        print(players)
     res = []
     for i in range(0, len(players)):
         name = players[i]['summonerName']
